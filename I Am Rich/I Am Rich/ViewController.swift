14545c9a4e425c0ead06bdce61b834a3313e16e2
Praveen <46050644+rprav-n@users.noreply.github.com>
2022-12-11 12:18:11 +0530

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Praveen on 10/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
